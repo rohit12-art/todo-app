8466531d36778f47266d6570ff9cab28da0a4fc0
Rohit <rohitrohan25420@gmail.com>
2022-06-16 12:43:04 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      {/* <TodoForm /> */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
